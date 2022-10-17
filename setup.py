1ba89ae10244e13faff30e1851224512812a2220
sheaffej <sheaffej@gmail.com>
2022-10-17 14:53:40 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'r2b2_base'
@@ -7,9 +9,9 @@ setup(
     version='2.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -19,7 +21,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'base_node = r2b2_base.base_node'
+            'base_node = r2b2_base.base_node:main'
         ],
     },
 )
